1efabdc25de4f3c3c206869368de442dde3623c6
youngSeok0413 <youngSeok0413@gmail.com>
2022-07-10 23:05:05 +0900

    Now redesigning Snake Class, Growth fuction added

--- SnakeGame/SnakeGame/main.cpp
+++ SnakeGame/SnakeGame/main.cpp
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <Windows.h>
-//#include <time.h>
+#include <list>
 #include <stack>
 
 #define WALL '#'
 #define SNAKE '$'
 #define FRUIT '@'
 #define BLANK ' '
-#define STAGESZ 50
+#define STAGESZ 25
 
 struct BlockInfo {
 	unsigned int Positions[2];
@@ -34,11 +34,21 @@ public:
 	ObjectPhysics(char blockType ,unsigned int x, unsigned int y) {
 		this->Block.Positions[0] = x;
 		this->Block.Positions[1] = y;
-		this->Block.PrevPositions[0] = 0;
-		this->Block.PrevPositions[1] = 0;
+		this->Block.PrevPositions[0] = STAGESZ + 10;
+		this->Block.PrevPositions[1] = STAGESZ + 10;
 		this->Block.Vectors[0] = 0;
-		this->Block.Vectors[1] = 1;
+		this->Block.Vectors[1] = 0;
 		this->Block.BlockType = blockType;
+	}
+
+	ObjectPhysics(BlockInfo info) {
+		this->Block.Positions[0] = info.PrevPositions[0];
+		this->Block.Positions[1] = info.PrevPositions[1];
+		this->Block.PrevPositions[0] = (info.PrevPositions[0] - info.Vectors[0]) > 0 ? info.PrevPositions[0] - info.Vectors[0] : 50;
+		this->Block.PrevPositions[1] = (info.PrevPositions[1] - info.Vectors[1]) > 0 ? info.PrevPositions[1] - info.Vectors[1] : 50;
+		this->Block.Vectors[0] = info.Vectors[0];
+		this->Block.Vectors[1] = info.Vectors[1];
+		this->Block.BlockType = info.BlockType;
 	}
 	
 	void setBlockPos(unsigned int x, unsigned int y) {
@@ -143,6 +153,52 @@ public:
 
 };
 
+//class snake should be redesigned
+class Snake {
+private:
+	std::list<ObjectPhysics> snake;
+public:
+	Snake() {
+		this->snake.push_back(ObjectPhysics(SNAKE, 5, 5));
+		this->snake.begin()->blockMoveUp();
+	}
+
+	void snakeGrow() {
+		BlockInfo buffer = this->snake.back().getBlockInfo();
+
+		this->snake.push_back(ObjectPhysics(SNAKE, buffer.PrevPositions[0], buffer.PrevPositions[1]));
+
+		if (buffer.Vectors[0] == 1) {
+			this->snake.back().blockMoveLeft();
+			this->snake.back().blockMoveRight();
+		}
+		else if (buffer.Vectors[0] == -1) {
+			this->snake.back().blockMoveRight();
+			this->snake.back().blockMoveLeft();
+		}
+		else {
+			if (buffer.Vectors[1] == 1) {
+				this->snake.back().blockMoveUp();
+				this->snake.back().blockMoveDown();
+			}
+			else if (buffer.Vectors[1] == -1) {
+				this->snake.back().blockMoveDown();
+				this->snake.back().blockMoveUp();
+			}
+		}
+	}
+
+	std::stack<BlockInfo> getSnakeInfo() {
+		std::stack<BlockInfo> buffer;
+		
+		for (std::list<ObjectPhysics>::iterator iter = this->snake.begin(); iter != this->snake.end(); iter++) {
+			buffer.push((*iter).getBlockInfo());
+		}
+
+		return buffer;
+	}
+};
+
 class Fruit : public ObjectPhysics{
 private : 
 	unsigned int Fx;
@@ -164,9 +220,11 @@ public :
 			Fy++;
 	}
 
-	void moveRandomly() {
-		this->getRandomPos();
-		this->setBlockPos(this->Fx, this->Fy);
+	void moveRandomly(bool snakeAteFruit) {
+		if (snakeAteFruit) {
+			this->getRandomPos();
+			this->setBlockPos(this->Fx, this->Fy);
+		}
 	}
 };
 
@@ -216,7 +274,21 @@ public:
 		this->toErase.push({info.PrevPositions[0], info.PrevPositions[1]});
 	}
 
-	void updateScreen() {
+	void updateFruitStack(BlockInfo& info) {
+		this->toDraw.push({ info.Positions[0], info.Positions[1], info.BlockType });
+		this->toErase.push({ info.PrevPositions[0], info.PrevPositions[1] });
+	}
+
+	void updateSnakeStack(std::stack<BlockInfo> snake) {
+		while (!snake.empty()) {
+			this->updateStacks(snake.top());
+			snake.pop();
+		}
+	}
+
+	void updateScreen(std::stack<BlockInfo> snake){
+		this->updateSnakeStack(snake);
+
 		while (!this->toErase.empty()) {
 			this->eraseBlock(this->toErase.top());
 			this->toErase.pop();
@@ -226,23 +298,28 @@ public:
 			this->printBlock(this->toDraw.top());
 			this->toDraw.pop();
 		}
+
+		while (!this->toDraw.empty()) {
+			this->toDraw.pop();
+		}
+
+		while (!this->toErase.empty()) {
+			this->toErase.pop();
+		}
 	}
 };
 
 int main() {
-	
-	Fruit fruit(5, 5);
+	Fruit fruit(5,5);
+	Snake snake;
 	Renderer ren;
 	BlockInfo buffer;
-	buffer = fruit.getBlockInfo();
-	ren.updateStacks(buffer);
 
 	while (1) {
-		ren.updateScreen();
-		fruit.moveRandomly();
-		buffer = fruit.getBlockInfo();
-		ren.updateStacks(buffer);
-		Sleep(1000);
+		ren.updateScreen(snake.getSnakeInfo());
+		snake.snakeGrow();
+		Sleep(500);
 	}
+	
 	return 0;
 }
